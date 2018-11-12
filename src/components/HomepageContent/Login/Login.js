a02027830e64ab758df313a84c346dde6ad588fe
tortoise777 <zenglei211@gmail.com>
2018-11-12 12:35:35 +0100

    Added a login form

--- src/components/HomepageContent/Login/Login.js
+++ src/components/HomepageContent/Login/Login.js
@@ -1,17 +1,53 @@
 import React, { Component } from 'react';
 
-import {Button} from "semantic-ui-react";
+import {Button, Form, Input, Segment, Grid} from "semantic-ui-react";
 
 import fakeAuth from '../../../service/fakeAuth';
 
 import { Redirect, withRouter } from 'react-router-dom';
 
+
 class Login extends Component {
 
-  state = {
-    redirectToReferrer: false
-  };
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      redirectToReferrer: false,
+
+      fields: {
+        email:"",
+        password: ""
+      }
+
+    };
+
+    this.changeHandler = this.changeHandler.bind(this);
+
+    this.submit = this.submit.bind(this);
+
+  }
+
+
+  changeHandler(evt, data) {
+    let name = data.name;
+    let value = data.value;
+
+    let fields = this.state.fields;
+    fields[name] = value;
+
+    this.setState({fields:fields});
+
+  }
+  
+  submit() {
+
+    //TODO: Axios call (localhost:5000/api/login)
+
+    console.log("Submit");
+  }
+  
+  
   login = () => {
     fakeAuth.authenticate(() => {
       this.setState({ redirectToReferrer: true });
@@ -27,7 +63,39 @@ class Login extends Component {
 
     const LoginCode = withRouter(({history}) => <Button size='huge' color='green' onClick={() => this.login(history)} >Login</Button>);
 
-    return <LoginCode/>;
+    //return <LoginCode/>;
+
+    console.log(this.state.fields);
+      
+      
+    return (
+
+      <Segment style={{ padding: '8em 0em' }} vertical>
+        <Grid container stackable verticalAlign='middle'>
+          <Grid.Row>
+            <Grid.Column width={8}>
+
+              <Form onSubmit={this.submit} >
+                <Form.Field>
+                  <label>Email</label>
+                  <Input name="email" placeholder='Email' onChange={this.changeHandler}/>
+                </Form.Field>
+                <Form.Field>
+                  <label>Password</label>
+                  <Input name="password" placeholder='Password' onChange={this.changeHandler}/>
+                </Form.Field>
+                <Button type='submit'>Login</Button>
+              </Form>
+
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
+
+
+
+    );
+
   }
 
 }
